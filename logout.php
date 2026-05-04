923ae04aecae748feda4abb8fa8c1389ea55476e
Your Name <njniran456@gmail.com>
2026-05-05 03:03:46 +0530

    Updated files

--- logout.php
+++ logout.php
@@ -7,8 +7,6 @@ $_SESSION = [];
 // Destroy the session
 session_destroy();
 
-// Do NOT delete cookies - theme and username cookies remain
-// for convenience on next visit (as per spec)
 
 // Redirect to login
 header('Location: login.php');
